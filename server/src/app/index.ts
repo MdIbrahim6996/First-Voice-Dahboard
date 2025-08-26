856d678431e0d62e3fd2fcf7b9614dd4ab791065
Ibrahim <mdibrahimhaf6996@gmail.com>
2025-08-26 23:07:50 +0530

    added ejs templates and styles.

--- server/src/app/index.ts
+++ server/src/app/index.ts
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import router from "./routes";
 import { errorHandler, notFound } from "./middlewares/errorHandler";
 import path from "path";
+import expressLayouts from "express-ejs-layouts";
 
 import cluster from "cluster";
 import os from "os";
@@ -23,28 +24,50 @@ app.use(
         optionsSuccessStatus: 200,
     })
 );
-
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(expressLayouts);
 // VIEWS
 app.set("view engine", "ejs");
-app.set("views", path.join(path.resolve(), "src/app/views/pages"));
+app.set("views", path.join(path.resolve(), "src/app/views"));
+app.set("layout", "layouts/main");
 
-app.use(express.static(path.join(path.resolve(), "src/app/views/pages/scss")));
-console.log(path.join(path.resolve(), "src/app/views/pages/scss"));
+//STATIC FILES
+app.use(express.static(path.join(path.resolve(), "src/app/public")));
+app.use("/js", express.static(path.join(path.resolve(), "src/app/public/js")));
+app.use(
+    "/css",
+    express.static(path.join(path.resolve(), "src/app/public/css"))
+);
+// console.log(path.join(path.resolve(), "src/app/views/pages/scss"));
 
 //ROUTES
+app.get("/user", (_, res: Response) => res.redirect("/user/dashboard"));
 app.get("/user/dashboard", (_, res: Response) =>
-    res.render("dashboard", { message: "ok" })
+    res.render("pages/dashboard", { currentPath: "/user/dashboard" })
 );
+app.get("/user/attendance", (_, res: Response) =>
+    res.render("pages/attendance", { currentPath: "/user/attendance" })
+);
+app.get("/user/holiday", (_, res: Response) =>
+    res.render("pages/holiday", { currentPath: "/user/holiday" })
+);
+app.get("/user/notification", (_, res: Response) =>
+    res.render("pages/notification", { currentPath: "/user/notification" })
+);
+app.get("/user/profile", (_, res: Response) =>
+    res.render("pages/profile", { currentPath: "/user/profile" })
+);
+// app.get("/user/{*any}", (_, res: Response) => res.send("pages not found"));
 app.get("/api/v1/health-check", (_, res: Response) =>
     res.send({ message: "ok" })
 );
 app.use("/api/v1", router);
 
+// Serve front-end app for all unmatched routes
 if (process.env.NODE_ENV === "production") {
-    // Serve front-end app for all unmatched routes
     app.use(express.static(path.join(path.resolve(), "../client", "dist")));
 
     app.get("/{*any}", (req, res) => {
@@ -54,16 +77,29 @@ if (process.env.NODE_ENV === "production") {
     });
 }
 
+// Serve front-end app for all unmatched routes
 app.use(express.static(path.join(path.resolve(), "../client", "dist")));
-
-app.get("/{*any}", (req, res) => {
+app.get("/app", (req, res) => {
     res.sendFile(
         path.resolve(path.resolve(), "../client", "dist", "index.html")
     );
 });
 
+app.use((req, res, next) => {
+    // Only handle 404s for non-React routes
+    if (req.path.startsWith("/app")) {
+        return next(); // let React handle it
+    }
+
+    res.status(404).render("pages/404", {
+        url: req.originalUrl,
+        layout: false, // optional: show requested URL
+    });
+});
+
 //ERROR HANDLER
 app.use(notFound);
+
 app.use(errorHandler);
 
 const PORT = 4000;
