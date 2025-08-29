e4d1b8058c43a352cac33a0d118b0cbc30679321
Ibrahim <mdibrahimhaf6996@gmail.com>
2025-08-29 23:01:23 +0530

    added comments for debugging purposes.

--- server/src/app/controllers/user/leads.controller.ts
+++ server/src/app/controllers/user/leads.controller.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { prisma } from "../../lib/prismaClient";
 import { Prisma } from "@prisma/client";
-
+// added comment 
 export const getUserLeads = async (
   req: Request,
   res: Response,
